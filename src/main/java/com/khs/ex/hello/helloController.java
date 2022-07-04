e738f800231fb15e73b94f8acc60f50d786991f1
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-07-04 21:44:37 +0900

    [김효석]  -Spring 설치 및 workspace 설정

---
+++ src/main/java/com/khs/ex/hello/helloController.java
@@ -0,0 +1,15 @@
+package com.khs.ex.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
