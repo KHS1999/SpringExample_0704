d19e3f6bd931bdd70b582cee449bad0aa1b439b6
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-07-11 21:40:51 +0900

    [김효석]  - spring insert 예제 문제 풀이

--- src/main/java/com/khs/ex/mybatis/ReviewController.java
+++ src/main/java/com/khs/ex/mybatis/ReviewController.java
@@ -29,5 +29,34 @@ public class ReviewController {
 		
 		return review;
 	}
-
+	
+	@ResponseBody
+	@RequestMapping("mybatis/ex01/2")
+	public String addReview() {
+		
+		// 리뷰 한행 저장
+		// storeId, menu, userName, point, review
+		// 4, "콤비네이션 피자, "김바다", 4.5, "할인도 많이 받고 잘 먹었습니다"
+		
+		int count = reviewBO.addReview(4, "콤비네이션 피자","김바다", 4.5, "할인도 많이 받고 잘 먹었습니다.");
+		
+		return "삽입 결과 : " + count;
+	}
+	@ResponseBody
+	@RequestMapping("mybatis/ex02/2")
+	public String addReviewByObject() {
+		// 2, 뿌링클, "김인규", 4.0, "역시 뿌링클은 진리 입니다!!"
+		
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리 입니다!");
+		
+		int count = reviewBO.addReviewByObject(review);
+		
+		return "삽입 결과 : " + count;
+	}
+	
 }
