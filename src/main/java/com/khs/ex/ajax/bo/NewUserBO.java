2d302ed7cb549b7b3273ebeb09978a33086ebf39
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-07-21 21:46:46 +0900

    [김효석]  - AJAX

---
+++ src/main/java/com/khs/ex/ajax/bo/NewUserBO.java
@@ -0,0 +1,28 @@
+package com.khs.ex.ajax.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.khs.ex.ajax.dao.NewUserDAO;
+import com.khs.ex.ajax.model.NewUser;
+
+@Service
+public class NewUserBO {
+	
+	@Autowired
+	private NewUserDAO newUserDAO;
+	
+	// new_user 테이블 모든 내용 얻어오기
+	public List<NewUser> getUserList(){
+		 return newUserDAO.selectUserList();
+	}
+	
+	public int addUser(String name, String birthday, String introduce, String email) {
+		
+		return newUserDAO.insertUser(name, birthday, introduce, email);
+		
+	}
+	
+}
